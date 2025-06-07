1d1cfcae3fd212d241fb37a31625f295e23bc64c
Pavlina Koritkova <pkoritkova@gmail.com>
2025-06-07 18:54:55 +0200

    algoritmus insert vrchniDily vs spodniDilya vice versa

---
+++ src/lib/match.test.js
@@ -0,0 +1,23 @@
+import { expect, test } from 'vitest';
+import { select, matchType, findData, filterData } from './match.js';
+
+test('matchType', () => {
+  expect(
+    matchType(findData({ typ: 'halenka' }), findData({ typ: 'kalhoty' })),
+  ).toBeTruthy();
+  expect(
+    matchType(findData({ typ: 'halenka' }), findData({ typ: 'halenka' })),
+  ).toBeFalsy();
+  expect(
+    matchType(findData({ typ: 'halenka' }), findData({ typ: 'kabat' })),
+  ).toBeFalsy();
+  expect(
+    matchType(findData({ typ: 'halenka' }), findData({ typ: 'trika' })),
+  ).toBeFalsy();
+});
+
+test('Co se páruje s id=14', () => {
+  const result = select(findData({ id: 14 }));
+  console.log(result);
+  //expect(sum(1, 2)).toBe(3)
+});
