9309dcfa44ab26c7b9bfff4a434fd1bf632a20c1
Pavlina Koritkova <pkoritkova@gmail.com>
2025-06-08 16:11:52 +0200

    zobrazeni selectedItemPage

--- src/pages/SelectPage/selectPage.jsx
+++ src/pages/SelectPage/selectPage.jsx
@@ -2,6 +2,7 @@ import { Header } from '../../components/Header/Header';
 import data from '../../../api/data.json';
 import './selectPage.css';
 import { Filter } from '../../components/Filter/Filter';
+import { Link } from 'react-router-dom';
 
 export const SelectPage = () => {
   return (
@@ -11,7 +12,9 @@ export const SelectPage = () => {
       <div className="get_data">
         {data.map((item) =>
           item.img ? (
-            <img key={item.id} src={`/fotky/${item.img}`}></img>
+            <Link to={'/selected-item/' + item.id} key={item.id}>
+              <img src={`/fotky/${item.img}`} />
+            </Link>
           ) : null,
         )}
       </div>
---
+++ src/pages/SelectedItemPage/selectedItemPage.jsx
@@ -0,0 +1,26 @@
+import './selectedItemPage.css';
+import { Header } from '../../components/Header/Header';
+import { useParams } from 'react-router-dom';
+import { findData, select } from '../../lib/match';
+
+export const SelectedItemPage = () => {
+  const { id } = useParams();
+  const selectedItem = findData({ id: Number(id) });
+  const data = select(selectedItem);
+  return (
+    <>
+      <Header />
+      <img className="selectedItem" src={`/fotky/${selectedItem.img}`} />
+      <h2>Hodí se k sobě:</h2>
+      <div className="get_data">
+        {data.length > 0
+          ? data.map((item) =>
+              item.img ? (
+                <img key={item.id} src={`/fotky/${item.img}`}></img>
+              ) : null,
+            )
+          : 'Nic tu neni'}
+      </div>
+    </>
+  );
+};
