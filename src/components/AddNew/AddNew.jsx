34c79775249033cf8ded75d1b04f580e6960493c
Tereza Matousova <tereza.matousova@gmail.com>
2025-06-09 08:48:04 +0200

    carousel fix

--- src/components/AddNew/AddNew.jsx
+++ src/components/AddNew/AddNew.jsx
@@ -1,5 +1,5 @@
 import { Title } from '../Title/Title';
-import { Carousel } from '../Carousel/carousel';
+import { Carousel } from '../Carousel/Carousel';
 
 export const AddNew = () => {
   return (
