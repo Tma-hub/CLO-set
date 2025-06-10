7614a2cdb6ae7c6ddd29a3104335a19a827eb7f0
Tereza Matousova <tereza.matousova@gmail.com>
2025-06-10 07:19:27 +0200

    app logo header

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -8,30 +8,32 @@ import './style.css';
 
 export const HomePage = () => {
   return (
-    <div className="homepage-container">
-      <HeaderMainPage>
-        <Link to="/about-app">
-          <button className="about-app">
-            <h2>O aplikaci</h2>
-          </button>
-        </Link>
-      </HeaderMainPage>
+    <div>
+      <div className="homepage-container">
+        <HeaderMainPage>
+          <Link to="/about-app">
+            <button className="about-app">
+              <h2>O aplikaci</h2>
+            </button>
+          </Link>
+        </HeaderMainPage>
 
-      <div className="homepage">
-        <Weather />
-        <RandomPicks />
-        <AddNew />
-        <Link to="/select-page">
-          <button className="what__to__wear_btn">
-            <h1>Co si vezmeš dnes na sebe?</h1>
-          </button>
-        </Link>
-        <Favorites />
-        <Link to="/sustainability">
-          <button className="sustainability_btn">
-            <h3>Jak na udržitelný šatník?</h3>
-          </button>
-        </Link>
+        <div className="homepage">
+          <Weather />
+          <RandomPicks />
+          <AddNew />
+          <Link to="/select-page">
+            <button className="what__to__wear_btn">
+              <h1>Co si vezmeš dnes na sebe?</h1>
+            </button>
+          </Link>
+          <Favorites />
+          <Link to="/sustainability">
+            <button className="sustainability_btn">
+              <h3>Jak na udržitelný šatník?</h3>
+            </button>
+          </Link>
+        </div>
       </div>
     </div>
   );
